70cbb94a8c21696c39a8455b4b0aa8f1f68488c1
Pythoholic <boxitupsam@gmail.com>
2020-11-16 13:00:56 +0530

    update code for connection to s3

---
+++ awsclidemo/connect_to_s3.py
@@ -0,0 +1,43 @@
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+def create_bucket(bucket_name, region=None):
+    """Create an S3 bucket in a specified region
+
+    If a region is not specified, the bucket is created in the S3 default
+    region (us-east-1).
+
+    :param bucket_name: Bucket to create
+    :param region: String region to create bucket in, e.g., 'us-west-2'
+    :return: True if bucket created, else False
+    """
+
+    # Create bucket
+    try:
+        if region is None:
+            s3_client = boto3.client('s3')
+            s3_client.create_bucket(Bucket=bucket_name)
+        else:
+            s3_client = boto3.client('s3', region_name=region)
+            location = {'LocationConstraint': region}
+            s3_client.create_bucket(Bucket=bucket_name,
+                                    CreateBucketConfiguration=location)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+def list_buckets(region):
+    # Retrieve the list of existing buckets
+    s3 = boto3.client('s3', region_name=region)
+    response = s3.list_buckets()
+
+    # Output the bucket names
+    print('Existing buckets:')
+    for bucket in response['Buckets']:
+        print(f'  {bucket["Name"]}')
+
+create_bucket("my-test-bucket", "ap-south-1")
+list_buckets("ap-south-1")
